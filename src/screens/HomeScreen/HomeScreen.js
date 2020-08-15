88f9ee440a8333ba425b4baf8efa09287dacc912
Joncitone <jarreola7123@gmail.com>
2020-08-14 19:07:03 -0500

    added basic outline of all lists screen

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -18,6 +18,7 @@ export default function HomeScreen(props) {
   const userID = props.user.id;
 
   useEffect(() => {
+    //load all documents in entity collection belonging to user
     entityRef
       .where('authorID', '==', userID)
       .orderBy('createdAt', 'desc')
@@ -37,21 +38,30 @@ export default function HomeScreen(props) {
       );
   }, []);
 
+  //handlerFunction for when button gets clicked/pressed
   const onAddButtonPress = () => {
+    //run the following if the field isn't empty
     if (entityText && entityText.length > 0) {
+      //set timestamp
       const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+
+      //build object with document fields
       const data = {
         text: entityText,
         authorID: userID,
         createdAt: timestamp,
       };
+
+      //add new document to database
       entityRef
         .add(data)
         .then((_doc) => {
+          //promise chain to set local state to empty
           setEntityText('');
           Keyboard.dismiss();
         })
         .catch((error) => {
+          //catch statement for error-handling (promise rejection)
           alert(error);
         });
     }
@@ -83,6 +93,7 @@ export default function HomeScreen(props) {
           <Text style={styles.buttonText}>Add</Text>
         </TouchableOpacity>
       </View>
+      {/* Conditional rendering of entities */}
       {entities && (
         <View style={styles.listContainer}>
           <FlatList
--- src/screens/Lists/AllLIsts.js
+++ src/screens/Lists/AllLIsts.js
@@ -9,3 +9,30 @@ import {
 } from 'react-native';
 import styles from './styles';
 import { firebase } from '../../firebase/config';
+
+export default function AllLists(props) {
+  //reference to collection in firestore database
+  //lists will be a sub collection on user documents, so this reference will change
+  const listRef = firebase.firestore().collection('lists');
+
+  //userID passed down through props
+  const userID = props.user.id;
+
+  //useState Hook for functional component
+  const [state, setState] = useState('');
+
+  //useEffect Hook for initial loading of page resources upon mounting
+  useEffect(() => {
+    //see HomeScreen for example of loading information from firestore
+  }, []);
+
+  const onAddList = () => {
+    //actions to proceed from Add List button being clicked
+  };
+
+  const onClickList = () => {
+    //actions to proceed from Clicking and Existing list
+  };
+
+  return <div>All Lists Screen</div>;
+}
